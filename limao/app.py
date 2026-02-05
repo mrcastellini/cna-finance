5df49ba83024e47d5c3021ef316ae64b84ee6349
mrcastellini <mistercastellini@hotmail.com>
2026-02-05 16:39:53 -0300

    Update app.py

--- limao/app.py
+++ limao/app.py
@@ -43,6 +43,8 @@ def health_check():
 
     try:
         return jsonify({"status": "alive"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -207,4 +209,5 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
 
     app.run(host='0.0.0.0', port=port)
+
 
